67b71513492c6b76afc2ca08144e2d8d111dc03b
nasa-stars <120693840+nasa-stars@users.noreply.github.com>
2024-11-22 11:33:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4086,7 +4086,7 @@ var SignJWT = class extends ProduceJWT {
 
 // src/worker.js
 var userID = "89b3cbba-e6ac-485a-9481-976a0415eab9";
-var trojanPassword = `bpb-trojan`;
+var trojanPassword = `dw!04.153dm,ui`;
 var proxyIPs = ["bpb.yousef.isegaro.com"];
 var defaultHttpPorts = ["80", "8080", "2052", "2082", "2086", "2095", "8880"];
 var defaultHttpsPorts = ["443", "8443", "2053", "2083", "2087", "2096"];
